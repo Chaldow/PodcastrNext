eedc4e47d879f3796512f50f676c753f0f6e86d3
Chaldow <nunesaxa@gmail.com>
2021-05-09 21:55:59 -0300

    NLW Podcastr React

---
+++ podcastrnext/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/global.scss'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+import { PLayerContextProvider } from '../contexts/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+return(
+  <PLayerContextProvider>
+    <div className={styles.wrapper}>
+      <main>
+      <Header />
+      <Component {...pageProps} />
+      </main>
+      <Player />
+      </div>
+    </PLayerContextProvider>
+  )
+}
+
+export default MyApp
